d8802a1c238fc9c50243f6d3eb75a77a5c2bff30
shreyashetty787 <85830028+shreyashetty787@users.noreply.github.com>
2021-06-15 22:23:29 +0530

    Add files via upload

---
+++ task 4 python bootcamp.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# a)Divide a by 10
+# b)multiply b by 50
+# c) add c by 60
+
+# In[4]:
+
+
+a=b=c=564
+print("a=",a,"b=",b,"c=",c)
+print("a divded by 10 is :",a/10)
+print("b multilplied by 50 is :",b*50)
+print("c added by 60 is :",c+60)
+
+
+# Create a string variable of 5 character and replace the third character with G
+# 
+
+# In[2]:
+
+
+string="sequel"
+print(string)
+print("after replacing G in the string",string.replace("q","G"))
+
+
+# Create two values (a,b) of int ,float datatype &convert the viseversa
+# 
+
+# In[6]:
+
+
+x=5656
+y=45.7878
+print("x=",x)
+print("after typecasting x to float=",float(x))
+print("y=",y)
+print("after type casting y to int",int(y))
+
+
+# In[ ]:
+
+
+
+
